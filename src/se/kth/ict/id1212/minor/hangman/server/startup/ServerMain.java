ec0c8c8517b32820892aacb7147463a42e78a46a
mikaelnorberg <mjnorberg@gmail.com>
2017-11-09 22:22:03 +0100

    blabla

--- src/se/kth/ict/id1212/minor/hangman/server/startup/ServerMain.java
+++ src/se/kth/ict/id1212/minor/hangman/server/startup/ServerMain.java
@@ -8,7 +8,7 @@ public class ServerMain {
     public static void main(String[] args){
         Word_DB words = new Word_DB("words.txt");
         try {
-            Server server = new Server(Integer.parseInt(args[0]), words);
+            new Server(Integer.parseInt(args[0]), words);
 
         } catch (NumberFormatException e){
             System.out.println("Portnummer måste vara en integer");
